6292a24e87866b5aea4090a5bdc4b7427fe3fdb6
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-07-13 19:07:55 -0300

    correçoes de texto e minificando style no build

--- gulpfile.js
+++ gulpfile.js
@@ -111,7 +111,8 @@ gulp.task('html', function () {
         .pipe(htmlMin({
             sortAttributes: true,
             sortClassName: true,
-            collapseWhitespace: true
+            collapseWhitespace: true,
+            minifyCSS: true
         }))
         .pipe(gulp.dest(config.dist))
 });
